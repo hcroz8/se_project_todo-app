fc86b944cf5303a4f2f12a397f9c161c3ca24759
Hezekiah Crosby <hcrosby67@gmail.com>
2025-03-26 09:03:14 -0400

    my updated todo project

---
+++ components/FormValidator.js
@@ -0,0 +1,13 @@
+class FormValidator {
+    constructor (settings, formEL) {
+        this._inputSelector = settings.inputSelector;
+        this._submitButtonSelector = settings.submitButtonSelector;
+        this._errorClass = settings.errorClass;
+        this._inputErrorClass = settings.inputErrorClass;
+        this._inactiveButtonClass = settings.inactiveButtonClass;
+        this._formEl = formEl;
+    }
+    enableValidation() {}
+}
+
+export default FormValidator;
